7162471ac0aa106ac43fedf439a88db9f6677c60
Roberto Ribeiro <robertov82008@gmail.com>
2021-09-06 15:16:55 -0300

    Testando a struct de Order

---
+++ test/orders/order_test.exs
@@ -0,0 +1,48 @@
+defmodule Exlivery.Orders.OrderTest do
+  use ExUnit.Case
+
+  alias Exlivery.Orders.Order
+
+  import Exlivery.Factory
+
+  describe "build/2" do
+    test "when all params are valid, returns an order" do
+      user = build(:user)
+
+      items = [
+        build(:item),
+        build(:item,
+          description: "Temaki de atum",
+          category: :japonesa,
+          quantity: 2,
+          unity_price: Decimal.new("20.50")
+        )
+      ]
+
+      response = Order.build(user, items)
+      expected_response = {:ok, build(:order)}
+
+      assert response == expected_response
+    end
+  end
+
+  test "when there is not items in the order, returns an error" do
+    user = build(:user)
+
+    items = []
+
+    response = Order.build(user, items)
+
+    expected_response = {:error, "Invalid parameters"}
+
+    assert response == expected_response
+  end
+
+  # test "when there is an invalid category, returns an error" do
+  #   response = Item.build("Pizza de peperoni", :banana, "35.5", 1)
+
+  #   expected_response = {:error, "Invalid parameters"}
+
+  #   assert response == expected_response
+  # end
+end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,7 +2,7 @@ defmodule Exlivery.Factory do
   use ExMachina
 
   alias Exlivery.Users.User
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
 
   def user_factory do
     %User{
@@ -20,6 +20,23 @@ defmodule Exlivery.Factory do
       description: "Pizza de peperoni",
       quantity: 1,
       unity_price: Decimal.new("35.5")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Rua x",
+      items: [
+        build(:item),
+        build(:item,
+          description: "Temaki de atum",
+          category: :japonesa,
+          quantity: 2,
+          unity_price: Decimal.new("20.50")
+        )
+      ],
+      total_price: Decimal.new("76.50"),
+      user_cpf: "123456789"
     }
   end
 end
